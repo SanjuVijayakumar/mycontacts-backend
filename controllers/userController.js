3ce947d572c48335af12a50c2f9571498e80a933
sanju <sanjukalathil05@gmail.com>
2026-08-11 02:18:46 +0530

    created userSchema and partially generated UserRegistration

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,26 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 
 //@desc Register a user
 //@route GET /api/users/register
 //@access public
 const registerUser = asyncHandler (async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const userAvailable = await User.findOne({ email });
+    if(userAvailable) {
+        res.status(400);
+        throw new Error("User already registered!");
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed password: ", hashedPassword);
+    
     res.json({message: "Register the user"});
 });
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,21 +1,31 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
+// import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    username: { 
+        type: String, 
+        required: [true, "Please add the user name"] 
+    },
+    email: { 
+        type: String, 
+        required: [true, "Please add the user name"], 
+        unique: [true, "Email address already taken"] 
+    },
+    password: { 
+        type: String, 
+        required: [true, "Please add the user password"] 
+    },
   },
   { timestamps: true }
 );
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+//   this.password = await bcrypt.hash(this.password, 10);
+//   next();
+// });
 
 // Compare password method
 userSchema.methods.matchPassword = async function (enteredPassword) {
