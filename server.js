05037c11b2c0146844c85bd34f92db05e2420033
sanju <sanjukalathil05@gmail.com>
2026-08-10 08:20:37 +0530

    created errorHandler.js and constants.js file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const dotenv = require("dotenv").config();
 const router = require("./routes/contactRoutes");
+const errorHandler = require("./middleware/errorhandler");
 
 const app = express()
 
@@ -8,8 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
-
 app.use("/api/contacts", router);
+app.use(errorHandler);
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
